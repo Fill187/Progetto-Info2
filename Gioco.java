f84a41dab7c15221534e72bab2e929b1fff49224
Niki151 <nicholas.corazza@allievi.bearzi.it>
2026-01-30 12:56:49 +0100

    Update Gioco.java

--- Gioco.java
+++ Gioco.java
@@ -28,75 +28,107 @@ public class Gioco {
             System.out.println("Impossibile aggiungere ulteriori giocatori. Limite raggiunto");
         }
     }
+    public void avviaPartita() {
+        if (giocatoriAttuali < 2) {
+            System.out.println("Servono almeno 2 giocatori per iniziare.");
+            return;
+        }
+
+        System.out.println("La partita è iniziata");
+
+        while (!partitaFinita) {
+            eseguiTurno();
+            controllaGiocatori();
+            controllaFinePartita();
+        }
+    }
+
+    private void eseguiTurno() {
+        Giocatore g = giocatori[turnoCorrente];
+
+        if (g.isEliminato()) {
+            passaTurno();
+            return;
+        }
+
+        System.out.println("\nTurno di: " + g.getNome());
+
+        int tiro = dadi.lancia();
+        System.out.println(g.getNome() + " tira i dadi: " + tiro);
+
+        g.muovi(tiro, tabellone.getNumeroCaselle());
+
+        Casella casella = tabellone.getCasella(g.getPosizione());
+        System.out.println(g.getNome() + " è finito su: " + casella.getNome());
+
+        gestisciCasella(g, casella);
+
+        passaTurno();
+    }
+
+    private void gestisciCasella(Giocatore g, Casella casella) {
+        if (casella instanceof Terreno) {
+            Terreno t = (Terreno) casella;
+
+
+            if (t.getProprietario() == null) {
+                if (g.getSoldi() >= t.getCosto()) {
+                    System.out.println(" Vuoi acquistare il terreno?");
+
+                    g.paga(t.getCosto());
+                    t.setProprietario(g);
+                    g.aggiungiTerreno(t);
+
+                    System.out.println(g.getNome() + " ha acquistato " + t.getNome());
+                } else {
+                    System.out.println(g.getNome() + " non ha abbastanza soldi per acquistare il terreno.");
+                }
+            }
+
+            else if (t.getProprietario() != g) {
+                int affitto = t.getAffitto();
+                System.out.println("Il terreno appartiene a " + t.getProprietario().getNome() +
+                        ". Affitto da pagare: " + affitto);
+
+                g.paga(affitto);
+                t.getProprietario().ricevi(affitto);
+
+                if (g.getSoldi() <= 0) {
+                    g.setEliminato(true);
+                    System.out.println(g.getNome() + " è stato eliminato per bancarotta!");
+                }
+            }
+        }
+    }
+
+    private void passaTurno() {
+        turnoCorrente = (turnoCorrente + 1) % giocatoriAttuali;
+    }
+
+    private void controllaGiocatori() {
+        for (Giocatore g : giocatori) {
+            if (g != null && g.getSoldi() <= 0) {
+                g.setEliminato(true);
+            }
+        }
+    }
+
+    private void controllaFinePartita() {
+        int attivi = 0;
+        Giocatore ultimo = null;
+
+        for (Giocatore g : giocatori) {
+            if (g != null && !g.isEliminato()) {
+                attivi++;
+                ultimo = g;
+            }
+        }
+
+        if (attivi == 1) {
+            partitaFinita = true;
+            System.out.println("\n Il vincitore è: " + ultimo.getNome());
+        }
+    }
     
-    /*public void muovi(Giocatore g, int passi) {
-        Casella pos = g.getPosizione();
-
-        for (int i = 0; i < passi; i++) {
-            pos = pos.getSuccessiva();
-            if (pos == tabellone.getStart()) {
-                System.out.println(g.getNome() + " passa dal VIA e prende 200€");
-                g.incassa(200);
-            }
-        }
-
-        g.setPosizione(pos);
-    }
-
-    public void mandaInPrigione(Giocatore g) {
-        g.vaiInPrigione(tabellone.getPrigione());
-    }
-
-    public void esegui() {
-        int turno = 0;
-
-        while (numeroGiocatori > 1) {
-
-            Giocatore g = giocatori.get(turno);
-
-            System.out.println("\nTurno di " + g.getNome());
-            System.out.println(g);
-
-            if (g.isInPrigione()) {
-                g.aggiornaPrigione();
-                turno++;
-                if (turno >= numeroGiocatori) turno = 0;
-                continue;
-            }
-
-            System.out.println("Premi invio per lanciare i dadi");
-            Leggi.unoString();
-
-            int[] lancio = dadi.lancia();
-            int somma = lancio[0] + lancio[1];
-
-            System.out.println("Hai fatto " + lancio[0] + " e " + lancio[1]);
-
-            muovi(g, somma);
-
-            System.out.println("Ora sei su: " + g.getPosizione().getNome());
-
-            g.getPosizione().azione(this, g);
-
-            if (g.morto()) {
-                System.out.println(g.getNome() + " è fallito ed esce dal gioco");
-                banca.riprendiTerreniDaGiocatore(g);
-                giocatori.remove(g);
-                numeroGiocatori--;
-
-                if (turno >= numeroGiocatori)
-                    turno = 0;
-
-                continue;
-            }
-
-            turno++;
-            if (turno >= numeroGiocatori)
-                turno = 0;
-        }
-
-        if (numeroGiocatori == 1) {
-            System.out.println("Vince " + giocatori.get(0).getNome());
-        }
-    }*/
+    
 }

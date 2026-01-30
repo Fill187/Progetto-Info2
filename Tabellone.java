ba22c225408577b1e742266e539968ee67bebfca
Niki151 <nicholas.corazza@allievi.bearzi.it>
2026-01-30 18:46:04 +0100

    Update Tabellone.java

--- Tabellone.java
+++ Tabellone.java
@@ -4,30 +4,29 @@ public class Tabellone {
     private int numeroCaselle;
 
     public Tabellone() {
-        caselle=new Casella[24];
-        numeroCaselle=0;
+        caselle = new Casella[24];
+        numeroCaselle = 0;
     }
 
     public void aggiungiCasella(Casella c) {
-    if (numeroCaselle == 0) {
-        caselle[0] = c;
-        c.setPrecedente(c);
-        c.setSuccessiva(c);
+        if (numeroCaselle == 0) {
+            caselle[0] = c;
+            c.setPrecedente(c);
+            c.setSuccessiva(c);
+            numeroCaselle++;
+            return;
+        }
+        Casella prima = caselle[0];
+        Casella ultima = caselle[numeroCaselle - 1];
+        ultima.setSuccessiva(c);
+        c.setPrecedente(ultima);
+        c.setSuccessiva(prima);
+        prima.setPrecedente(c);
+        caselle[numeroCaselle] = c;
         numeroCaselle++;
-        return;
     }
-    Casella prima = caselle[0];
-    Casella ultima = caselle[numeroCaselle - 1];
-    ultima.setSuccessiva(c);
-    c.setPrecedente(ultima);
-    c.setSuccessiva(prima);
-    prima.setPrecedente(c);
-    caselle[numeroCaselle] = c;
-    numeroCaselle++;
-}
 
-
-    public Casella getCasellaIniziale(){
+    public Casella getCasellaIniziale() {
         return caselle[0];
     }
 
@@ -39,13 +38,11 @@ public class Tabellone {
         return numeroCaselle;
     }
 
-    public void stampaTabellone(){
+    public void stampaTabellone() {
         System.out.println("TABELLONE DI GIOCO");
-        System.out.println("---------------------------------------------");
-        System.out.println("---------------------------------------------");
-        for(int cont1=0;cont1<caselle.length;cont1++){
+        
+        for (int cont1 = 0; cont1 < numeroCaselle; cont1++) {
             System.out.println(caselle[cont1].toString());
-            System.out.println("---------------------------------------------");
             System.out.println("---------------------------------------------");
         }
     }
